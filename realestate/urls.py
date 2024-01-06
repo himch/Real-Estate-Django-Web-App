8fcf381039de7bbee150ed0156becf78194c6c72
himch <skhimchenko@gmail.com>
2024-01-06 21:18:09 +0300

    dadi4

--- realestate/urls.py
+++ realestate/urls.py
@@ -15,4 +15,4 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.MEDIA_URL_1, document_root=settings.MEDIA_ROOT_1)
 
-urlpatterns = i18n_patterns(urlpatterns)
+# urlpatterns = i18n_patterns(urlpatterns)
