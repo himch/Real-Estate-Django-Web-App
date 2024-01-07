fd3b3689bc9b8f251f81477b9eee88ca84fd285b
himch <skhimchenko@gmail.com>
2024-01-07 17:01:07 +0300

    dadi9

--- pages/views.py
+++ pages/views.py
@@ -100,7 +100,14 @@ def katalogi(request):
     # Get MVP
     mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
 
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     context = {
+        'listings': paged_listings,
         'realtors': realtors,
         'mvp_realtors': mvp_realtors
     }
