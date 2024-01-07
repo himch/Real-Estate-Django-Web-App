e63eb63f05131376ed954b878c6fda35c3f83e0a
himch <skhimchenko@gmail.com>
2024-01-08 01:17:47 +0300

    dadi12

--- listings/views.py
+++ listings/views.py
@@ -22,7 +22,7 @@ def index(request):
 
 
 def listing(request, listing_id):
-    listing = get_object_or_404(Listing, pk=listing_id)
+    listing_item = get_object_or_404(Listing, pk=listing_id)
 
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
@@ -30,11 +30,79 @@ def listing(request, listing_id):
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
-    amenities = json.loads(listing.listing_amenities)
+    # amenities_type = {('air', 'conditioner'): 'air-conditioner',
+    #                   ('balcony', ): 'balcony',
+    #                   ('balcony', ): 'bed',
+    #                   'celebrate',
+    #                   'clock',
+    #                   'clothes-hanger',
+    #                   'dishwasher',
+    #                   'dont-smoke',
+    #                   'door',
+    #                   'element',
+    #                   'farming',
+    #                   'guard',
+    #                   'key-chain',
+    #                   'location-pin',
+    #                   'monitor',
+    #                   'parking',
+    #                   'paw',
+    #                   'profile-2user',
+    #                   'shower',
+    #                   'slider',
+    #                   'swimming-pool',
+    #                   'tray',
+    #                   'wardrobe',
+    #                   'weight',
+    #                   'wifi'
+    #                   }
+    amenities = dict()
+    for lang in ('ru', 'en', 'ar'):
+        amenities[lang] = [amenity[lang] for amenity in json.loads(listing_item.listing_amenities) if lang in amenity]
+
+    br_prices = json.loads(listing_item.listing_br_prices)
+    print(br_prices)
+    if br_prices:
+        br_price = br_prices[0]
+        currency = br_price['currency']
+        min_price = float(br_price['min_price'])
+        min_price_rub = convert(min_price, currency, 'RUB')
+        min_price_usd = convert(min_price, currency, 'USD')
+        min_price_eur = convert(min_price, currency, 'EUR')
+        min_price_m2 = float(br_price['min_price_m2'])
+        min_price_m2_rub = convert(min_price_m2, currency, 'RUB')
+        min_price_m2_usd = convert(min_price_m2, currency, 'USD')
+        min_price_m2_eur = convert(min_price_m2, currency, 'EUR')
+        min_price_ft2_rub = round(min_price_m2_rub / 10.7639)
+        min_price_ft2_usd = round(min_price_m2_usd / 10.7639)
+        min_price_ft2_eur = round(min_price_m2_eur / 10.7639)
+        min_area_m2 = round(float(br_price['min_area']['m2']))
+    else:
+        min_area_m2 = min_area_ft2 = None
+        min_price_rub = min_price_usd = min_price_eur = None
+        min_price_m2_rub = min_price_m2_usd = min_price_m2_eur = None
+        min_price_ft2_rub = min_price_ft2_usd = min_price_ft2_eur = None
 
     context = {
-        'amenities': amenities,
-        'listing': listing,
+        'br_prices': br_prices,
+        'min_area_m2': min_area_m2,
+
+        'min_price_rub': f"{min_price_rub:,}",
+        'min_price_usd': f"{min_price_usd:,}",
+        'min_price_eur': f"{min_price_eur:,}",
+
+        'min_price_m2_rub': f"{min_price_m2_rub:,}",
+        'min_price_m2_usd': f"{min_price_m2_usd:,}",
+        'min_price_m2_eur': f"{min_price_m2_eur:,}",
+
+        'min_price_ft2_rub': f"{min_price_ft2_rub:,}",
+        'min_price_ft2_usd': f"{min_price_ft2_usd:,}",
+        'min_price_ft2_eur': f"{min_price_ft2_eur:,}",
+
+        'amenities_ru': amenities['ru'],
+        'amenities_en': amenities['en'],
+        'amenities_ar': amenities['ar'],
+        'listing': listing_item,
         'listings': paged_listings
     }
 
@@ -84,3 +152,11 @@ def search(request):
     }
 
     return render(request, 'listings/search.html', context)
+
+
+def convert(value, currency1, currency2):
+    courses = {'AEDRUB': 24.76,
+               'AEDUSD': 0.27,
+               'AEDEUR': 0.25
+               }
+    return round(value * courses[currency1 + currency2])
