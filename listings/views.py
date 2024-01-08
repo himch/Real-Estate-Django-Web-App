bb4eb3ee92428bceeba0131264fd778aa058e0d3
himch <skhimchenko@gmail.com>
2024-01-08 11:51:35 +0300

    dadi15

--- listings/views.py
+++ listings/views.py
@@ -24,38 +24,14 @@ def index(request):
 def listing(request, listing_id):
     listing_item = get_object_or_404(Listing, pk=listing_id)
 
+    realtor = listing_item.realtor
+
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
     paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
-    # amenities_type = {('air', 'conditioner'): 'air-conditioner',
-    #                   ('balcony', ): 'balcony',
-    #                   ('balcony', ): 'bed',
-    #                   'celebrate',
-    #                   'clock',
-    #                   'clothes-hanger',
-    #                   'dishwasher',
-    #                   'dont-smoke',
-    #                   'door',
-    #                   'element',
-    #                   'farming',
-    #                   'guard',
-    #                   'key-chain',
-    #                   'location-pin',
-    #                   'monitor',
-    #                   'parking',
-    #                   'paw',
-    #                   'profile-2user',
-    #                   'shower',
-    #                   'slider',
-    #                   'swimming-pool',
-    #                   'tray',
-    #                   'wardrobe',
-    #                   'weight',
-    #                   'wifi'
-    #                   }
     amenities = dict()
     for lang in ('ru', 'en', 'ar'):
         amenities[lang] = [amenity[lang] for amenity in json.loads(listing_item.listing_amenities) if lang in amenity]
@@ -102,7 +78,8 @@ def listing(request, listing_id):
         'amenities_en': amenities['en'],
         'amenities_ar': amenities['ar'],
         'listing': listing_item,
-        'listings': paged_listings
+        'listings': paged_listings,
+        'realtor': realtor
     }
 
     # return render(request, 'listings/listing.html', context)
@@ -159,3 +136,31 @@ def convert(value, currency1, currency2):
                'AEDEUR': 0.25
                }
     return round(value * courses[currency1 + currency2])
+
+
+# amenities_type = {('air', 'conditioner'): 'air-conditioner',
+#                   ('balcony', ): 'balcony',
+#                   ('balcony', ): 'bed',
+#                   'celebrate',
+#                   'clock',
+#                   'clothes-hanger',
+#                   'dishwasher',
+#                   'dont-smoke',
+#                   'door',
+#                   'element',
+#                   'farming',
+#                   'guard',
+#                   'key-chain',
+#                   'location-pin',
+#                   'monitor',
+#                   'parking',
+#                   'paw',
+#                   'profile-2user',
+#                   'shower',
+#                   'slider',
+#                   'swimming-pool',
+#                   'tray',
+#                   'wardrobe',
+#                   'weight',
+#                   'wifi'
+#                   }
