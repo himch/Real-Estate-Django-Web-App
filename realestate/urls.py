b8172bc6d49f47771c34886b5342e80229940f9d
himch <skhimchenko@gmail.com>
2024-01-16 01:36:10 +0300

    dadi 25

--- realestate/urls.py
+++ realestate/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls import include, url
+from django.conf.urls import include
 
 # admin.autodiscover()
 
